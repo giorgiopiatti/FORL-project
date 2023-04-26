749dc0e503c35f01f4f105f1faf404e205aa59a2
Giorgio Piatti <contact@giorgiopiatti.ch>
2023-04-26 13:50:33 +0200

    refactor

--- enviroment/briscola/__init__.py
+++
@@ -1,5 +0,0 @@
-from enviroment.briscola.dealer import BriscolaDealer as Dealer
-from enviroment.briscola.game import BriscolaGame as Game
-from enviroment.briscola.judger import BriscolaJudger as Judger
-from enviroment.briscola.player import BriscolaPlayer as Player
-from enviroment.briscola.round import BriscolaRound as Round
